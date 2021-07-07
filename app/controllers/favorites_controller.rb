98aacf724c233c9e9c0273de52622093e02eb775
EC2 Default User <ec2-user@ip-172-31-84-171.ec2.internal>
2021-07-07 09:02:32 +0000

    ♥のアイコン実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,15 @@
 class FavoritesController < ApplicationController
   def create
-    @favorite = Favorite.
-  end  
+    @book = Book.find(params[:book_id])
+    @favorite = Favorite.new(user_id: current_user.id, book_id: @book.id)
+    @favorite.save
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    @book = Book.find(params[:book_id])
+    @favorite = Favorite.find_by(user_id: current_user.id, book_id: @book.id)
+    @favorite.destroy
+    redirect_back(fallback_location: root_path)
+  end
 end
