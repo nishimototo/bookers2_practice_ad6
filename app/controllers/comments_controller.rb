9fff022424548510df100b2b0a978ab9200d1594
nishimototo <dkspm2436@gmail.com>
2021-07-10 12:30:45 +0000

    [Finish]ad6

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,20 @@
 class CommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.comments.new(comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @comment = current_user.comments.new(comment_params)
+    @comment.book_id = @book.id
+    @comment.save
+    render :index
+    #redirect_back(fallback_location: root_path)
 
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    Comment.find_by(id: params[:id], book_id: book.id).destroy
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @comment = Comment.find_by(id: params[:id], book_id: @book.id)
+    @comment.destroy
+    render :index
+    #redirect_back(fallback_location: root_path)
   end
 
   private
